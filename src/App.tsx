e027b9f29f0a17256eea42eb2a5a71748b4b86e7
Kevin Burton <burtonator@gmail.com>
2024-04-17 16:48:41 -0600

    OK this mostly works for just bold now... I need to support the other markdown options next.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './App.css';
 
 import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
@@ -7,8 +7,31 @@ import {RichTextPlugin} from '@lexical/react/LexicalRichTextPlugin';
 import {ContentEditable} from '@lexical/react/LexicalContentEditable';
 import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
 import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import {OnChangePlugin} from "@lexical/react/LexicalOnChangePlugin";
+
+import {
+  MarkdownShortcutPlugin
+} from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import {EditorRefPlugin} from "@lexical/react/LexicalEditorRefPlugin";
+import {LexicalEditor} from "lexical";
+import {
+  $convertToMarkdownString,
+  BOLD_STAR,
+} from "@lexical/markdown";
 
 const theme = {
+
+  text: {
+    bold: 'lexical-text-bold',
+    italic: 'lexical-text-italic',
+    underline: 'lexical-text-underline',
+    code: 'lexical-text-code',
+    highlight: 'lexical-text-highlight',
+    strikethrough: 'lexical-text-strikethrough',
+    subscript: 'lexical-text-subscript',
+    superscript: 'lexical-text-superscript',
+  },
+
 
 }
 
@@ -18,7 +41,11 @@ const initialConfig = {
   onError: () => console.error("Some error or something!"),
 };
 
+const myTransformers = [BOLD_STAR]
 function App() {
+
+  const editorRef = useRef<LexicalEditor>(null);
+
   return (
     <div className="App">
       <LexicalComposer initialConfig={initialConfig}>
@@ -28,7 +55,17 @@ function App() {
           ErrorBoundary={LexicalErrorBoundary}
         />
         <HistoryPlugin />
+        <MarkdownShortcutPlugin transformers={myTransformers}/>
         <AutoFocusPlugin />
+        <EditorRefPlugin editorRef={editorRef} />
+        <OnChangePlugin onChange={editorState => {
+
+          editorState.read(() => {
+            const markdown = $convertToMarkdownString(myTransformers);
+            console.log(markdown);
+          });
+        }} />
+
       </LexicalComposer>
     </div>
   );
